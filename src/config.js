bc5ae1a3456609de71099fe09d2c3edfa0edcca5
MoritzRohleder <92987315+MoritzRohleder@users.noreply.github.com>
2025-05-13 12:45:15 +0200

    Create config.js
    constants are now saved in here to make them globally available
    
    Signed-off-by: MoritzRohleder <92987315+MoritzRohleder@users.noreply.github.com>

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// The configuration file for storing constants and settings
+export const WEBHOOKS = {
+  news: "https://discord.com/api/webhooks/...",
+  changelog: "https://discord.com/api/webhooks/...",
+  suggestions: "https://discord.com/api/webhooks/..."
+};
+
+export const PINGS = {
+  news: "<@&1297538431001432135>",
+  monthly: "<@&1346200306911940639>",
+  release: "<@&1297543002222493761>"
+};
+
+// Avatar URL to use in Discord messages
+export const AVATAR_URL = "https://drive.google.com/uc?id=YOUR_AVATAR_IMAGE_ID";
+
+// A default text to use for footer in messages
+export const FOOTER_TEXT = "This post originates from GitHub.";
