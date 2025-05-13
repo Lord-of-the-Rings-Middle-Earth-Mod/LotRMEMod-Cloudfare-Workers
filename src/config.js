4e209f4695f4a617b02935dea7602e66f0d48ea8
MoritzRohleder <92987315+MoritzRohleder@users.noreply.github.com>
2025-05-13 13:32:08 +0200

    Update config.js
    added a webhook for the fabricblog channel and updated the other webhook links
    
    Signed-off-by: MoritzRohleder <92987315+MoritzRohleder@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 // The configuration file for storing constants and settings
 export const WEBHOOKS = {
-  news: "https://discord.com/api/webhooks/...",
-  changelog: "https://discord.com/api/webhooks/...",
-  suggestions: "https://discord.com/api/webhooks/..."
+  news: "https://discord.com/api/webhooks/1344295085797277728/IkpbMIPwm68YxjD0xG1fSUheYeAQUsa1PuiBjebiOaSvAAiJvYMXeW3aTi6QaB0w4gKg",
+  changelog: "https://discord.com/api/webhooks/1301097428606517268/Z6R6lt5g2hKpUZMaBT7ro-o3d9-YZdpPrkbDzux6ubw8H227ykoCXwO19F-wlIus-b5-",
+  suggestions: "https://discord.com/api/webhooks/1301226095584084059/_VRnk4v15GjEa_poXiWI27gTk00_3NB2AQxhheCtYE191RivFNxWsjJNEZXEkIdY-qz2",
+  fabricblog: "https://discord.com/api/webhooks/1371811039045488640/2t7wVqLMCoowz54ctIwuTh4UTOG2uASATLBrKkh_UbCotZpX61T7qdnPd95BOYA6yV4o"
 };
 
 export const PINGS = {
