3f3ec6d5269df13ffa888922c2efd671cf00bbf0
MoritzRohleder <92987315+MoritzRohleder@users.noreply.github.com>
2025-05-27 13:14:14 +0200

    Update config.js
    Signed-off-by: MoritzRohleder <92987315+MoritzRohleder@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ export const WEBHOOKS = {
   news: "https://discord.com/api/webhooks/1344295085797277728/IkpbMIPwm68YxjD0xG1fSUheYeAQUsa1PuiBjebiOaSvAAiJvYMXeW3aTi6QaB0w4gKg",
   changelog: "https://discord.com/api/webhooks/1301097428606517268/Z6R6lt5g2hKpUZMaBT7ro-o3d9-YZdpPrkbDzux6ubw8H227ykoCXwO19F-wlIus-b5-",
   suggestions: "https://discord.com/api/webhooks/1301226095584084059/_VRnk4v15GjEa_poXiWI27gTk00_3NB2AQxhheCtYE191RivFNxWsjJNEZXEkIdY-qz2",
-  fabricblog: "https://discord.com/api/webhooks/1371811039045488640/2t7wVqLMCoowz54ctIwuTh4UTOG2uASATLBrKkh_UbCotZpX61T7qdnPd95BOYA6yV4o"
+  fabricblog: "https://discord.com/api/webhooks/1371811039045488640/2t7wVqLMCoowz54ctIwuTh4UTOG2uASATLBrKkh_UbCotZpX61T7qdnPd95BOYA6yV4o",
+  mails: "https://discord.com/api/webhooks/1376881118363783179/Z861kL7YymjgnE5ohlbx2fpy4zS94XNaiB8Yzw88qjNB7JBBU_Rcwsp10krcihIwB_Wc"
 };
 
 export const PINGS = {
